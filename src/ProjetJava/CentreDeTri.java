d020ea8e29164ef0dcc1971b260411cbda26d068
XR <huangxre@gmail.com>
2025-03-10 15:01:18 +0100

    2

--- src/ProjetJava/CentreDeTri.java
+++ src/ProjetJava/CentreDeTri.java
@@ -3,4 +3,28 @@ package ProjetJava;
 public class CentreDeTri {
     private String nom;
     private String addresse;
+
+    public CentreDeTri() {
+    }
+
+    public CentreDeTri(String nom, String adresse) {
+        this.nom = nom;
+        this.adresse = adresse;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getAddresse() {
+        return addresse;
+    }
+
+    public void setAddresse(String addresse) {
+        this.addresse = addresse;
+    }
 }
