16985c57116d85514dffeadf246aa4e649593d2e
XinRuHUANG <huangxre@gmail.com>
2025-03-22 16:04:03 +0100

    Delete src/main/PoubelleIntelligente directory

--- src/main/PoubelleIntelligente/PoubelleIntelligentePlastiques.java
+++
@@ -1,8 +0,0 @@
-package main.PoubelleIntelligente;
-
-public class PoubelleIntelligentePlastiques extends PoubelleIntelligente {
-    public static final int capaciteMaximalePlastiques = 100;
-    public PoubelleIntelligentePlastiques(int identifiant, String emplacement) {
-        super(identifiant, emplacement, capaciteMaximalePlastiques);
-    }
-}
