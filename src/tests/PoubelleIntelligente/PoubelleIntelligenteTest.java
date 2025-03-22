55641c8e1e5fe3efaa6e6735da62652311e24c82
Anthusan <anthusan.s@gmail.com>
2025-03-22 19:32:37 +0100

    Update PoubelleIntelligenteTest.java

--- src/tests/PoubelleIntelligente/PoubelleIntelligenteTest.java
+++ src/tests/PoubelleIntelligente/PoubelleIntelligenteTest.java
@@ -1,4 +1,18 @@
-package tests.PoubelleIntelligente;
-
 public class PoubelleIntelligenteTest {
+    public static void main(String[] args) {
+        // Création d’une poubelle intelligente
+        PoubelleIntelligente poubelle = new PoubelleIntelligente(
+            1,
+            "Rue des Lilas",
+            50.0f,
+            TypeDechet.plastique
+        );
+
+        // Appel de quelques méthodes à tester
+        poubelle.ajouterPoubelle();
+        poubelle.mesurerVolume();
+        poubelle.collecterDechets();
+        poubelle.statistiquerDechets();
+    }
 }
+
