4896b9842923368790c88db58a2467dd179c17c7
hajainakazz <116579259+hajainakazz@users.noreply.github.com>
2025-03-22 13:52:31 +0100

    Update PROJETJAVA.sql
    WARNING: sans les clés etrangeres et associations n-n !!!!!!!!

--- PROJETJAVA.sql
+++ PROJETJAVA.sql
@@ -3,68 +3,42 @@ CREATE DATABASE Recyclage;
 USE Recyclage;
 
 DROP TABLE IF EXISTS Commerce;
-DROP TABLE IF EXISTS Emplacement;
 DROP TABLE IF EXISTS CentreTri;
 DROP TABLE IF EXISTS PoubelleIntelligente;
-DROP TABLE IF EXISTS Corbeille;
-DROP TABLE IF EXISTS Compte;
-DROP TABLE IF EXISTS Menage;
+DROP TABLE IF EXISTS Depot;
+DROP TABLE IF EXISTS Utilisateur;
 DROP TABLE IF EXISTS Contrat;
-DROP TABLE IF EXISTS historiqueDepot;
+DROP TABLE IF EXISTS CategorieDeProduits;
+DROP TABLE IF EXISTS Dechet;
+DROP TABLE IF EXISTS Promotion;
 
-CREATE TABLE Emplacement(
-	idEmplacement INT AUTO_INCREMENT PRIMARY KEY,
-    	nomEmplacement VARCHAR(50),
-	latitude DECIMAL(12,10) NOT NULL,
-	longitude DECIMAL(13,10) NOT NULL,
-	CONSTRAINT check_latitude CHECK (latitude BETWEEN -90 AND 90),
-	CONSTRAINT check_longitude CHECK (longitude BETWEEN -180 AND 180));
 
 CREATE TABLE PoubelleIntelligente(
 	idPoubelle INT AUTO_INCREMENT PRIMARY KEY,
-        idEmplacement INT,
+        Emplacement String,
 	poidsMax DECIMAL,
         volume DECIMAL,
-        typePoubelle VARCHAR(10),
-        FOREIGN KEY (idEmplacement) REFERENCES Emplacement(idEmplacement));
+        typePoubelle VARCHAR(10);
 
 CREATE TABLE CentreTri(
 	idCentreTri INT AUTO_INCREMENT PRIMARY KEY,
-        idEmplacement INT,
+        Emplacement String,
 	nomCentreTri VARCHAR(30),
         FOREIGN KEY (idEmplacement) REFERENCES Emplacement(idEmplacement));
 
-CREATE TABLE Menage(
-	idMenage INT AUTO_INCREMENT PRIMARY KEY,
-        idEmplacement INT,
-	nomRepresentant INT ,
-	prenomRepresentant INT,
-        FOREIGN KEY (idEmplacement) REFERENCES Emplacement(idEmplacement));
- 
-CREATE TABLE Corbeille(
-	idCorbeille INT AUTO_INCREMENT PRIMARY KEY,
-	qtePlastique DECIMAL,
-	qteVerre DECIMAL,
-	qteCarton DECIMAL,
-	qteMetaux DECIMAL,
-        idMenage INT,
-        FOREIGN KEY (idMenage) REFERENCES Menage(idMenage));
-
-CREATE TABLE Compte(
-	idCompte INT AUTO_INCREMENT PRIMARY KEY, 
-        idMenage INT,
-	ptFidelite INT,
-        codeAcces INT,
-        FOREIGN KEY (idMenage) REFERENCES Menage(idMenage));
+CREATE TABLE Utilisateur(
+	idUtilisateur INT AUTO_INCREMENT PRIMARY KEY,
+	nom INT ,
+	prenom INT,
+	pointFidelite DECIMAL;
 
 CREATE TABLE Commerce(
 	idCommerce INT AUTO_INCREMENT PRIMARY KEY,
         nomCommerce VARCHAR(15),
-        idEmplacement INT,
-        FOREIGN KEY (idEmplacement) REFERENCES Emplacement(idEmplacement));	
+        Emplacement String);	
 
 CREATE TABLE Contrat(
-	idCommerce INT,
+	idCommerce INT AUTO_INCREMENT PRIMARY KEY,
         idCentreTri INT,
 	dateDebut DATE,
         dateFin DATE,
@@ -73,21 +47,23 @@ CREATE TABLE Contrat(
 	FOREIGN KEY fk_centre(idCentreTri) REFERENCES CentreTri(idCentreTri));
 
 CREATE TABLE Depot(
-	idPoubelle INT,
-        idCorbeille INT,
-        idCompte INT,
-        CONSTRAINT pk_Depot PRIMARY KEY (idPoubelle, idCorbeille, idCompte),
-        FOREIGN KEY (idCorbeille) REFERENCES Corbeille(idCorbeille), 
-	FOREIGN KEY (idPoubelle) REFERENCES PoubelleIntelligente(idPoubelle));
+	idDepot INT AUTO_INCREMENT PRIMARY KEY,
+        date DATE,
+	heure LocalTime,
+        points DECIMAL);
+
+CREATE TABLE Dechet(
+	idDechet INT AUTO_INCREMENT PRIMARY KEY,
+	type VARCHAR(10));
+
+CREATE TABLE CategorieDeproduits(
+	idCat INT AUTO_INCREMENT PRIMARY KEY,
+	nom String);
 
 CREATE TABLE Promotion(
-       idCommerce INT,
-       idCompte INT,
-       pourcentageRemise INT,
-       ptRequis INT,
-       CONSTRAINT pk_promotion PRIMARY KEY (idCommerce,idCompte),
-       FOREIGN KEY (idCommerce) REFERENCES Commerce(idCommerce),
-       FOREIGN KEY (idCompte) REFERENCES Compte(idCompte));
+	idPromotion INT,
+	pourcentageRemise DECIMAL,
+	ptRequis DECIMAL);
 
     
 
