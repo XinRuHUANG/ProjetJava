67ec40d3568821ab5ca7eabcb2d988985614127a
Anthusan <anthusan.s@gmail.com>
2025-03-22 19:35:32 +0100

    Update CommerceTest.java

--- src/tests/CommerceTest.java
+++ src/tests/CommerceTest.java
@@ -1,4 +1,14 @@
-package tests;
+public class TestCommerce {
+    public static void main(String[] args) {
+        // Création d’un commerce fictif
+        Commerce commerce = new Commerce(1, "ÉcoBoutique");
 
-public class CommerceTest {
+        // Appel des méthodes à tester
+        commerce.ajouterCommerce();
+        commerce.retirerCommerce();
+
+        // Affichage pour vérification
+        System.out.println("Commerce créé : " + commerce);
+    }
 }
+
