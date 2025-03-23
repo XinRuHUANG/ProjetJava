4097e3366709eec0fcc902b5c2d2beaece72eab2
XinRuHUANG <huangxre@gmail.com>
2025-03-23 19:25:55 +0100

    Update PoubelleIntelligenteTest.java

--- src/tests/PoubelleIntelligenteTest.java
+++ src/tests/PoubelleIntelligenteTest.java
@@ -1,17 +1,19 @@
+
+package tests;
+
+import main.PoubelleIntelligente;
+import main.TypeDéchetEnum;
+
 public class PoubelleIntelligenteTest {
-    public static void main(String[] args) {
+    public static void test() {
         // Création d’une poubelle intelligente
-        PoubelleIntelligente poubelle = new PoubelleIntelligente(
-            1,
-            "Rue des Lilas",
-            50.0f,
-            TypeDechet.plastique
-        );
+        PoubelleIntelligente poubelle = PoubelleIntelligente.ajouterPoubelle(TypeDéchetEnum.TypeDechet.plastique,"Rue des Lilas", 50.0f);
+
+        //Affichage
+        System.out.println("poubelle : " + poubelle);
 
         // Appel de quelques méthodes à tester
-        poubelle.ajouterPoubelle();
-        poubelle.mesurerVolume();
-        poubelle.collecterDechets();
-        poubelle.statistiquerDechets();
+        PoubelleIntelligente.collecterDechets(poubelle.getIdentifiantPoubelle());
+        poubelle.retirerPoubelle();
     }
 }
