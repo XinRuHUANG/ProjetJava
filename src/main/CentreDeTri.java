7f3a59dc6c3eef82ecce1af030bb8e79c427a434
EmineOA <emine.ouldagatt@gmail.com>
2025-04-25 17:51:13 +0200

    Update CentreDeTri.java

--- src/main/CentreDeTri.java
+++ src/main/CentreDeTri.java
@@ -49,13 +49,13 @@ public class CentreDeTri {
     }
 
     //Méthodes de classes
-    public static CentreDeTri ajouterCentre(int idCentreDeTri, String nom, String adresse, Set<PoubelleIntelligente> poubelleIntelligente, List<Commerce> commerce, List<Contrat> contrats){
-        CentreDeTri centreDeTri = new CentreDeTri(idCentreDeTri, nom, adresse, poubelleIntelligente, commerce, contrats);
+    public static CentreDeTri ajouterCentre( String nom, String adresse, Set<PoubelleIntelligente> poubelleIntelligente, List<Commerce> commerce, List<Contrat> contrats){
+        CentreDeTri centreDeTri = new CentreDeTri(0, nom, adresse, poubelleIntelligente, commerce, contrats);
         ajouterCentreBDD(centreDeTri);
         return centreDeTri;
     }
 
-    public void modifierCentre(CentreDeTri centredetri, Map<String, Object> modifications){
+    public void modifierCentre(Map<String, Object> modifications){
         /*Fonction pour modifier le depot, une map "modifications" permet d'informer le programme des attributs qu'on veut modifier, on suppose que cette map est de la forme
          * {ième attribut = ième valeur}*/
         for(Map.Entry<String, Object> entry: modifications.entrySet()) {
@@ -63,28 +63,30 @@ public class CentreDeTri {
             Object obj = entry.getValue();
             if (cle=="nom"){
                 String nom = (String) obj;
-                centredetri.setNom(nom);
-                actualiserCentreBDD(centredetri, cle);
+                this.setNom(nom);
+                actualiserCentreBDD(this, cle);
             }
             if (cle=="adresse"){
                 String adresse = (String) obj;
-                centredetri.setAdresse(adresse);
-                actualiserCentreBDD(centredetri, cle);
+                this.setAdresse(adresse);
+                actualiserCentreBDD(this, cle);
             }
             if (cle=="poubelles"){
                 Set<PoubelleIntelligente> poubelles = (Set<PoubelleIntelligente>) obj;
-                centredetri.setPoubelles(poubelles);
-                actualiserCentreBDD(centredetri, cle);
+                this.setPoubelles(poubelles);
+                actualiserCentreBDD(this, cle);
             }
             if (cle=="commerces"){
+                assert modifications.containsKey("contrats");
                 List<Commerce> commerces = (List<Commerce>) obj;
-                centredetri.setCommerce(commerce);
-                actualiserCentreBDD(centredetri, cle);
+                this.setCommerce(commerce);
+                actualiserCentreBDD(this, cle);
             }
             if (cle=="contrats"){
+                assert modifications.containsKey("commerces");
                 List<Contrat> contrats = (List<Contrat>) obj;
-                centredetri.setContrats(contrats);
-                actualiserCentreBDD(centredetri, cle);
+                this.setContrats(contrats);
+                actualiserCentreBDD(this, cle);
             }
         }
 
@@ -93,15 +95,27 @@ public class CentreDeTri {
     public void supprimerCentre(){
         supprimerCentreBDD(this);
     }
-    
+
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj) return true;
+        if (obj==null || getClass() != obj.getClass()) return false;
+        CentreDeTri centreDeTri = (CentreDeTri ) obj;
+        return this.idCentreDeTri == centreDeTri.idCentreDeTri &&
+                this.nom == centreDeTri.nom && this.adresse == centreDeTri.adresse
+                && this.poubelles.equals(centreDeTri.poubelles) && this.commerce.equals(centreDeTri.commerce)
+                && this.contrats.equals(centreDeTri.contrats);
+    }
+
     @Override
     public String toString() {
         return "CentreDeTri{" +
                 "idCentreDeTri=" + idCentreDeTri +
                 ", nom='" + nom + '\'' +
                 ", adresse='" + adresse + '\'' +
-                ", poubelleIntelligente=" + poubelles +
+                ", poubelles=" + poubelles +
                 ", commerce=" + commerce +
+                ", contrats=" + contrats +
                 '}';
     }
 }
