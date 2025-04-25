b5ba1cc25fdcbba9b42f7b990cf8f16cff0d3a55
EmineOA <emine.ouldagatt@gmail.com>
2025-04-25 19:29:36 +0200

    Update Dechet.java

--- src/main/Dechet.java
+++ src/main/Dechet.java
@@ -13,6 +13,7 @@ public class Dechet {
 
     //modélisation des associations
     private Depot contenir;
+    private PoubelleIntelligente stocker;
 
     //Getter et Setter
     public int getIdentifiantDechet() {
@@ -36,16 +37,20 @@ public class Dechet {
         this.contenir = contenir;
     }
 
+    public PoubelleIntelligente getStocker() {return stocker;}
+    public void setStocker(PoubelleIntelligente stocker) {this.stocker = stocker;}
+
     //Constructeur
-    public Dechet(int idDechet, TypeDechet type, Depot contenir) {
+    public Dechet(int idDechet, TypeDechet type, Depot contenir, PoubelleIntelligente stocker) {
         this.identifiantDechet = idDechet;
         this.type = type;
         this.contenir = contenir;
+        this.stocker = stocker;
     }
 
     //Methodes de Classes
-    public static Dechet ajouterDechet(TypeDechet type, Depot contenir) {
-        Dechet dechet = new Dechet(0, type, contenir);
+    public static Dechet ajouterDechet(TypeDechet type, Depot contenir, PoubelleIntelligente stocker) {
+        Dechet dechet = new Dechet(0, type, contenir, stocker);
         ajouterDechetBDD(dechet);
         return dechet;
     }
@@ -69,6 +74,11 @@ public class Dechet {
                 this.setContenir(depot);
                 actualiserDechetBDD(this, cle);
             }
+            if (cle=="stocker"){
+                PoubelleIntelligente stocker = (PoubelleIntelligente) obj;
+                this.setStocker(stocker);
+                actualiserDechetBDD(this, cle);
+            }
         }
     }
 
@@ -78,7 +88,7 @@ public class Dechet {
         if (obj==null || getClass() != obj.getClass()) return false;
         Dechet dechet = (Dechet) obj;
         return this.identifiantDechet == dechet.identifiantDechet &&
-                this.type == dechet.type && this.contenir.equals(dechet.contenir);
+                this.type == dechet.type && this.contenir.equals(dechet.contenir) && this.stocker.equals(dechet.stocker);
     }
 
     @Override
@@ -87,6 +97,7 @@ public class Dechet {
                 "identifiantDechet=" + identifiantDechet +
                 ", type=" + type +
                 ", contenir=" + contenir +
+                ", stocker=" + stocker +
                 '}';
     }
 }
